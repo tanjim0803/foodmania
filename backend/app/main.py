004b69991705120076de5e50d7ecac31067e7224
tanjim0803 <tanjimahmed1905@gmail.com>
2026-09-07 01:45:24 +0600

    added cors-middleware in main.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import routers
 
 app = FastAPI(
     title="FoodMania", description="A Food Delivery Management App", version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+ )
 
 
 @app.get("/")
